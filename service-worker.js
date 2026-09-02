c593000aafc0478dd121b32b2c3d39693be85590
Navesh <85452070+navesh05@users.noreply.github.com>
2026-09-02 17:04:41 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,10 @@
-const CACHE_NAME = 'tracker-pwa-v1';
+const CACHE_NAME = 'tracker-pwa-v2';
 const APP_SHELL = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
